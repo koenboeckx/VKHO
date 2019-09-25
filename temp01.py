97b5aad3c5f0ffd5c4d5fe7346a75c233e1399c3
koen <koen.boeckx@uliege.student.be>
2019-09-25 22:43:24 +0200

    Created directory

--- temp01.py
+++ temp01.py
@@ -1,7 +1,12 @@
+"""
+Initial attempt to implement EGT for symmetric games with 2 players.
+Visualition only enabled for 2-action games
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-DEBUG = True
+DEBUG = False
 ALPHA = 0.01
 
 ## Rock/Paper/Scissors
@@ -11,48 +16,37 @@ A = np.array([[ 0, -1,  1],     # actions = R | P | S
               [-1,  1,  0]])
 
 ## Stag Hunt
-
 A = np.array([[4, 1],
               [3, 3]])
 
-m, n = A.shape
-B = A.T
+## Prisoner's Dilemma
+A = np.array([[3, 0],
+              [5, 1]])
 
-x = np.random.dirichlet((1,)*m)
-y = np.random.dirichlet((1,)*m)
-
-trace_x, trace_y = [], []
-
-for i in range(1000):
-    dx = x * (x.dot(A) - x.dot(A).dot(y))
-    dy = y * (y.dot(B) - y.dot(B).dot(x))
-
-    x += ALPHA * dx
-    y += ALPHA * dy
-
-    # Normalize (due to rounding errors)
-    x /= sum(x)
-    y /= sum(y)
-
-        print(i, ' x = ', x)
-        print(i, ' y = ', y)
-
-    trace_x.append(x.copy())
-    trace_y.append(y.copy())
+## Matching Pennies
+""" A = np.array([[1, 0],
+              [0, 1]]) """
 
 def generate_trace(A, n_steps=1000):
+    """
+    A: payoff matrix for symmetric game
+    n_steps: number of iteration steps 
+    """
     m, n = A.shape
     B = A.T
 
+    # generate initial point in unit simplex for both players
     x = np.random.dirichlet((1,)*m)
     y = np.random.dirichlet((1,)*m)
 
     trace_x, trace_y = [], []
 
     for i in range(n_steps):
+        # replicator dynamics
         dx = x * (x.dot(A) - x.dot(A).dot(y))
         dy = y * (y.dot(B) - y.dot(B).dot(x))
 
+        # Euler integration
         x += ALPHA * dx
         y += ALPHA * dy
 
@@ -64,6 +58,7 @@ def generate_trace(A, n_steps=1000):
             print(i, ' x = ', x)
             print(i, ' y = ', y)
 
+        # add to trace
         trace_x.append(x.copy())
         trace_y.append(y.copy())
     
@@ -71,8 +66,8 @@ def generate_trace(A, n_steps=1000):
     trace_y = np.array(trace_y)
 
     return trace_x, trace_y
-        
-trace_x, trace_y = generate_trace(A)
 
-plt.plot(trace_x[:, 0], trace_y[:, 0])
+for i in range(100):
+    trace_x, trace_y = generate_trace(A)
+    plt.plot(trace_x[:, 0], trace_y[:, 0])
 plt.show()
