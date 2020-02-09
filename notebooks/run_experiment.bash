fbaea24ff4ace85c029f50e9e85c3a35567e5010
koen <koen.boeckx@uliege.student.be>
2020-02-09 09:51:35 +0100

    run experiment with bash script

---
+++ notebooks/run_experiment.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+#python3 q-learning.py
+#python3 q-learning.py with lr=0.01
+#python3 q-learning.py with n_hidden=128
+#python3 q-learning.py with lr=0.01 n_hidden=128
+#python3 q-learning.py with lr=0.1
+##python3 q-learning.py with batch_size=128
+#python3 q-learning.py with lr=0.1 batch_size=128
+#python3 q-learning.py with batch_size=128 n_hidden=128
+#python3 q-learning.py with batch_size=128 sync_rate=10
+#python3 q-learning.py with batch_size=128 sync_rate=50
+#python3 q-learning.py with batch_size=128 sync_rate=200
+python3 q-learning.py with lr=0.005 # best
+python3 q-learning.py with lr=0.01
+python3 q-learning.py with lr=0.05
