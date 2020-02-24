d9b0439e7640850c535cc91d783079f5d5b663c9
koen <koen.boeckx@uliege.student.be>
2020-02-24 17:13:50 +0100

    added target network to iac (no improvement)

--- new_env/iac.py
+++ new_env/iac.py
@@ -3,7 +3,9 @@ Independent Actor-Critic
 Uses independent A2C agents with common weights
 """
 
+import copy
 from collections import namedtuple
+
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -26,6 +28,10 @@ class IACAgent(Agent):
         
     def set_model(self, model):
         self.model = model
+        self.target_model = copy.deepcopy(model)
+    
+    def sync_models(self):
+        self.target_model.load_state_dict(self.model.state_dict())
     
     def act(self, obs):
         if not obs.alive: # if not alive, do nothing
@@ -40,6 +46,17 @@ class IACAgent(Agent):
 
             action_idx = Categorical(logits=logits[0]).sample().item()
         return self.actions[action_idx]
+    
+    def compute_returns(self, batch):
+        _, _, rewards, _, dones, _, _, _, _ = zip(*batch)
+        rewards = [reward[self.team] for reward in rewards]
+        returns, R = [], 0.0
+        for reward, done in reversed(list(zip(rewards, dones))):
+            if done:
+                R = 0.0
+            R = reward + args.gamma * R
+            returns.insert(0, R)
+        return returns
 
     def set_hidden_state(self):
         self.hidden_state = self.model.init_hidden()
@@ -53,8 +70,14 @@ class IACAgent(Agent):
         next_obs     = [obs[self] for obs in next_obs]
         dones = torch.tensor(dones, dtype=torch.float)
 
-        rewards  = torch.tensor([reward[self.team] for reward in rewards])
+        rewards = torch.tensor([reward[self.team] for reward in rewards])
+        returns = torch.tensor(self.compute_returns(batch))[self_alive_idx]
         value, logits = self.model(observations)
+
+        probs = F.softmax(logits, dim=1)
+        log_probs = F.log_softmax(logits, dim=1)
+        entropy = -(probs * log_probs).sum(dim=1)
+        mean_entropy = entropy.mean()
 
         actions = torch.tensor([action[self].id for action in actions])[self_alive_idx]
 
@@ -63,7 +86,7 @@ class IACAgent(Agent):
             unavail_ids = [action.id for action in unavail_actions]
             logits[idx][unavail_ids] = -99999
 
-        next_val, _ = self.model(next_obs)
+        next_val, _ = self.target_model(next_obs) # TODO: use target network and compare
         target = rewards + args.gamma * next_val * (1.0 - dones)
         advantage = target.detach() - value
         advantage = advantage[self_alive_idx]        
@@ -71,6 +94,7 @@ class IACAgent(Agent):
         log_prob = F.log_softmax(logits, dim=-1)
         log_prob_act = log_prob[self_alive_idx, actions]
         log_prob_act_val = advantage * log_prob_act
+        #log_prob_act_val = returns * log_prob_act
         
         loss_pol = -log_prob_act_val.mean()
         loss_val = advantage.pow(2).mean()
@@ -82,7 +106,8 @@ class IACAgent(Agent):
 
         return {'loss':         loss.item(),
                 'policy_loss':  loss_pol.item(),
-                'value_loss':   loss_val.item()
+                'value_loss':   loss_val.item(),
+                'entropy':      mean_entropy.item(),
         }
 
 def play_from_file(filename):
@@ -103,7 +128,7 @@ def generate_model(input_shape, n_actions):
 
 def train(args):
     team_blue = [IACAgent(idx, "blue") for idx in range(args.n_friends + 1)]
-    team_red  = [Agent(2 + idx, "red") for idx in range(args.n_enemies)]
+    team_red  = [Agent(args.n_friends + 1 + idx, "red") for idx in range(args.n_enemies)]
 
     training_agents = team_blue
 
@@ -144,6 +169,11 @@ def train(args):
             ex.log_scalar(f'policy_loss{agent.id}', loss['policy_loss'], step=n_episodes)
             ex.log_scalar(f'value_loss{agent.id}',  loss['value_loss'],  step=n_episodes)
             ex.log_scalar(f'loss{agent.id}', loss['loss'], step=n_episodes)
+            ex.log_scalar(f'entropy{agent.id}', loss['entropy'], step=n_episodes)
+
+            if step_idx % 50 == 0: #args.sync_interval == 0:
+                agent.sync_models()
+                print(f'sync at {step_idx * args.n_episodes_per_step}')
 
         s  = f"Step {step_idx}: "
         s += f"Average length: {epi_len/args.n_episodes_per_step:5.2f} - "
@@ -152,7 +182,8 @@ def train(args):
         epi_len, nwins = 0, 0
 
         #_ = generate_episode(env, render=True)
-    
+
+
     path = '/home/koen/Programming/VKHO/new_env/agent_dumps/'
     for agent in training_agents:
         agent.save(path+f'RUN_{get_run_id()}_AGENT{agent.id}.p')
